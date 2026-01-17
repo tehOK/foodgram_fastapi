6cc6e1417f2264aa2a54be887ee17d8a4a26b858
tehOK <mishak62@gmail.com>
2026-01-17 12:16:34 +0300

    add jwt and password utils

--- backend/auth/utils.py
+++ backend/auth/utils.py
@@ -1,21 +1,51 @@
+from datetime import datetime, timedelta
+
 import bcrypt
 import jwt
 
 from config import settings
 
 
-def encode_jwt():
-    pass
-    #encode = jwt.encode()
+
+def encode_jwt(
+    payload: dict,
+    private_key: str = settings.auth.private_jwt_path.read_text(),
+    algorithm: str = settings.auth.jwt_algorithm,
+    expire_minutes: int = settings.auth.access_token_lifetime,
+):
+    to_encode = payload.copy()
+    now = datetime.now()
+    expire = now + timedelta(minutes=expire_minutes)
+    to_encode.update(
+        #iat=now,
+        exp=expire,
+    )
+    encode = jwt.encode(
+        to_encode,
+        private_key,
+        algorithm,
+    )
+    return encode
 
 
-def decode_jwt():
-    pass
+def decode_jwt(
+    token: str | bytes,
+    public_key: str = settings.auth.public_jwt_path.read_text(),
+    algorithm: str = settings.auth.jwt_algorithm,
+):
+    decoded = jwt.decode(
+        token,
+        public_key,
+        algorithm,
+    )
+    return decoded
+
 
 def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     pwd_bytes: bytes = password.encode()
     return bcrypt.hashpw(pwd_bytes, salt)
+
 
 def verify_password(password: str, hashed_password: bytes) -> bool:
     return bcrypt.checkpw(
